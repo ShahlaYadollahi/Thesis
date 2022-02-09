016fe90899bcf00c63351138173e0198910c48c1
Shahla Yadollahi <19715463+ShahlaYadollahi@users.noreply.github.com>
2022-02-09 16:40:03 +0100

    Create README.m

---
+++ SMOS_Backscatter_relation/README.m
@@ -0,0 +1,3 @@
+The soil moisture for each pixel can thus be retrieved using the following function:
+Mv(i,j,NDVI,d)=(ΔσNDVI(i,j)/f(NDVI))(Mvmax−Mvmin)+Mvmin(i,j,d)
+SMOS low-resolution moisture products (SMOS Level 3 daily product), corresponding to the one-year period of S1 acquisitions, were used to estimate Mvmax and Mvmin, since we do not want to use any ground measurements. The mean S1 radar signal is estimated over a SMOS pixel (40 km × 40 km).
