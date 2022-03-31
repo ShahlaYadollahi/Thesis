f3e8d54ff7ffd975ec168fc1ee5c5f3ea89f46c7
Shahla Yadollahi <19715463+ShahlaYadollahi@users.noreply.github.com>
2022-03-31 19:15:36 +0200

    Update 5.delta_backscatter_with_ndvi.py

--- NDVI_Backscatter_relation_extraction/5.delta_backscatter_with_ndvi.py
+++ NDVI_Backscatter_relation_extraction/5.delta_backscatter_with_ndvi.py
@@ -12,12 +12,13 @@ import numpy as np
 import csv
 
 
-common= ['06/08/2020', '10/06/2020', '07/03/2020', '10/31/2020']
-common_dates = ['20200608', '20201006', '20200703', '20201031']
 
 ndvi_path = glob.glob(os.path.join("/theia/scratch/brussel/104/vsc10412/NDVI/", '*.tif'))
 s1_path = glob.glob(os.path.join("/theia/scratch/brussel/104/vsc10412/delta_backscattered_sentinel1/", '*.tif'))
+s1_dates=[f[95:103] for f in s1_path]
+ndvi_dates=[z[63:71] for z in ndvi_path]
 
+common_dates=list(set(s1_dates).intersection(ndvi_dates))
 s1_ndvi = {}
 xs=[]
 ys=[]
